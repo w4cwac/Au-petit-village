27566ca4f70bcf6ffe858958798e67c00323173b
wacwac <wacogne.hugo@gmail.com>
2024-05-09 14:02:22 +0200

    création du header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent],
   template: `
+    <app-header></app-header>
     <h1>Welcome to {{title}}!</h1>
 
     <router-outlet />
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   template: `
     <p>
       home works!
