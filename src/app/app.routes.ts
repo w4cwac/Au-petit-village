e133375d59f430fa387a88aa5a9dc1422ae1c351
wacwac <wacogne.hugo@gmail.com>
2024-05-09 12:07:19 +0200

    mise en place du router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProductComponent } from './product/product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'About', component: AboutComponent},
+    {path: 'Product', component: ProductComponent},
+
+];
